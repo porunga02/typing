ba48bb5807766d20fcedb735e0a9a88a69827ae8
porunga_02 <36418737+porunga01@users.noreply.github.com>
2019-06-02 18:21:25 +0900

    [Fixed] シーン遷移の追加

---
+++ typing/Assets/Script/LoadSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UniRx;
+
+public class LoadSceneManager : MonoBehaviour
+{
+	[SerializeField]
+	Button StartButton;
+	[SerializeField]
+	Button ExittButton;
+
+
+
+	void Start()
+	{
+		OnButtonDown();
+	}
+
+
+	void OnButtonDown()
+	{
+		StartButton.onClick.AsObservable().Subscribe(_ => SceneManager.LoadScene("main"));
+		ExittButton.onClick.AsObservable().Subscribe(x => Application.Quit());
+
+	}
+}
